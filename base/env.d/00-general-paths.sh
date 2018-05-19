a1fe755feb24cfc595dff33375abdf981ec54f53
Alex Corkwell <i.am.the.memory@gmail.com>
2018-05-18 22:50:14 -0400

    Add general system bin directories to the PATH

---
+++ base/env.d/00-general-paths.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Try to add general system bin directories, just in case they're missing, e.g.
+# sbin directories on some distros.
+
+for bindir in /bin /sbin /usr/bin /usr/sbin /usr/local/bin /usr/local/sbin
+do
+  PATH="$("${HOME}/.local/bin/munge.sh" "${PATH}" "${bindir}")"
+done
+
+export PATH
