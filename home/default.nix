6a9a05beee18bdd6b55c22398d2b144706662bec
Alexandria Corkwell <i.am.the.memory@gmail.com>
2018-12-28 11:52:21 -0800

    Split channels into another file

--- home/default.nix
+++ home/default.nix
@@ -1,36 +1,6 @@
 { config, lib, options, ... }:
 let
-  import-channel = c: (
-    import (
-      builtins.fetchTarball
-      "https://nixos.org/channels/nixos-${c}/nixexprs.tar.xz"
-    )
-  ) {};
-
-  import-branch = b: (
-    import (
-      builtins.fetchGit {
-        url = "https://github.com/nixos/nixpkgs.git";
-        ref = b;
-      }
-    )
-  ) {};
-
-  aardvark    = import-channel "13.10";
-  baboon      = import-channel "14.04";
-  caterpillar = import-channel "14.12";
-  dingo       = import-channel "15.09";
-  emu         = import-channel "16.03";
-  flounder    = import-channel "16.09";
-  gorilla     = import-channel "17.03";
-  hummingbird = import-channel "17.09";
-  impala      = import-channel "18.03";
-  jellyfish   = import-channel "18.09";
-
-  stable      = jellyfish;
-  unstable    = import-channel "unstable";
-
-  staging     = import-branch  "staging";
+  inherit (import ./channels.nix) stable unstable jellyfish staging;
 
   speedswapper = unstable.writeText "speedswapper" ''
     ! Swap caps lock and escape
