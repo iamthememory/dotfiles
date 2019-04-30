196132a6fa9eef68d4c4ce90a32afdf00394b1b8
Alexandria Corkwell <i.am.the.memory@gmail.com>
2019-04-30 11:30:48 -0700

    Fix PYTHONSTARTUP to be a string of the filepath

--- home/default.nix
+++ home/default.nix
@@ -266,35 +266,38 @@ in
         XMODIFIERS = "@im=ibus";
         _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel";
 
-        PYTHONSTARTUP = unstable.writeScript "pythonrc.py" ''
-          # This file is sourced by interactive python sessions
-          # Partially copied from <http://stackoverflow.com/questions/3613418/what-is-in-your-python-interactive-startup-script>
+        PYTHONSTARTUP = let
+          pythonrc = unstable.writeScript "pythonrc.py" ''
+            # This file is sourced by interactive python sessions
+            # Partially copied from <http://stackoverflow.com/questions/3613418/what-is-in-your-python-interactive-startup-script>
 
-          from __future__ import division, print_function
+            from __future__ import division, print_function
 
-          import atexit
-          import os
-          import readline
-          import rlcompleter
+            import atexit
+            import os
+            import readline
+            import rlcompleter
 
-          # Tab complete with readline
-          readline.parse_and_bind("tab: complete")
+            # Tab complete with readline
+            readline.parse_and_bind("tab: complete")
 
-          # History
-          historyPath = os.path.expanduser("~/.history.py")
+            # History
+            historyPath = os.path.expanduser("~/.history.py")
 
-          def save_history(historyPath=historyPath):
-              import readline
-              readline.write_history_file(historyPath)
+            def save_history(historyPath=historyPath):
+                import readline
+                readline.write_history_file(historyPath)
 
-          if os.path.exists(historyPath):
-              readline.read_history_file(historyPath)
+            if os.path.exists(historyPath):
+                readline.read_history_file(historyPath)
 
-          atexit.register(save_history)
-          del os, atexit, readline, rlcompleter, save_history, historyPath
+            atexit.register(save_history)
+            del os, atexit, readline, rlcompleter, save_history, historyPath
 
-          # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-        '';
+            # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+          '';
+        in
+          "${pythonrc}";
 
         LS_COLORS = let
           solarized = builtins.fetchGit {
