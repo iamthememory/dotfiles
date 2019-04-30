4dec9740226400248fe574af361c970f98eb22c7
Alexandria Corkwell <i.am.the.memory@gmail.com>
2019-04-30 11:41:07 -0700

    Enable all languages for vim

--- overlays/iamthememory/packages/vim_configurable.nix
+++ overlays/iamthememory/packages/vim_configurable.nix
@@ -1,4 +1,17 @@
 { self, super }:
   with super; super.vim_configurable.override {
+    cscopeSupport = true;
+    features = "huge";
+    ftNixSupport = true;
+    guiSupport = "gtk3";
+    luaSupport = true;
+    multibyteSupport = true;
+    nlsSupport = true;
+    perlSupport = true;
+    pythonSupport = true;
+    rubySupport = true;
+    tclSupport = true;
+    ximSupport = true;
+
     python = pkgs.python3Full;
   }
