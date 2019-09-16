f480b63eaffca161bd9d28ba32347bbba00ae049
Alexandria Corkwell <i.am.the.memory@gmail.com>
2019-09-16 16:15:30 -0400

    Disable a git test that fails on ZFS

--- overlays/iamthememory/packages/gitAndTools/default.nix
+++ overlays/iamthememory/packages/gitAndTools/default.nix
@@ -1,4 +1,14 @@
 { self, super }:
+let
+  patchgit = pkg: pkg.overrideAttrs (oldAttrs: {
+    preInstallCheck = oldAttrs.preInstallCheck + ''
+      # Fails on ZFS.
+      disable_test t3910-mac-os-precompose
+    '';
+  });
+in
   (super.gitAndTools or {}) // {
-    gitFull = import ./gitFull.nix { inherit self super; };
+    gitFull = patchgit (import ./gitFull.nix { inherit self super; });
+    git = patchgit super.gitAndTools.git;
+    gitSVN = patchgit super.gitAndTools.gitSVN;
   }
