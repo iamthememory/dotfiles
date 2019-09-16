9ad0a1d1ee87915251d8c5f527ed20da518ad1f3
Alexandria Corkwell <i.am.the.memory@gmail.com>
2019-09-16 14:33:39 -0400

    Enable all retroarch emulators

--- config.nix
+++ config.nix
@@ -10,4 +10,35 @@ with import <nixpkgs> {};
       enablePepperFlash = true;
       #enableWideVine = true;
     };
+
+    retroarch = {
+      enable4do = true;
+      enableBeetlePCEFast = true;
+      enableBeetlePSX = true;
+      enableBeetleSaturn = true;
+      enableBsnesMercury = true;
+      enableDesmume = true;
+      enableDolphin = true;
+      enableFBA = true;
+      enableFceumm = true;
+      enableGambatte = true;
+      enableGenesisPlusGX = true;
+      enableHiganSFC = true;
+      enableMAME = true;
+      enableMGBA = true;
+      enableMupen64Plus = true;
+      enableNestopia = true;
+      enableParallelN64 = true;
+      enablePicodrive = true;
+      enablePrboom = true;
+      enablePPSSPP = true;
+      enableQuickNES = true;
+      enableReicast = true;
+      enableScummVM = true;
+      enableSnes9x = true;
+      enableSnes9xNext = true;
+      enableStella = true;
+      enableVbaNext = true;
+      enableVbaM = true;
+    };
   }
