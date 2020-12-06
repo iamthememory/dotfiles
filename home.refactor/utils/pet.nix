bcdedc954e98d0456764a07e3461e269cb1a8e80
Alexandria Corkwell <i.am.the.memory@gmail.com>
2020-12-06 05:50:39 -0500

    feat(utils/pet): Add a snippet to clone the inputs of a flake

--- home.refactor/utils/pet.nix
+++ home.refactor/utils/pet.nix
@@ -32,5 +32,17 @@
       command = "cd ~/dotfiles && nix build \".#homeManagerConfigurations.$(hostname).activationPackage\" && ./result/activate";
       description = "Build and activate a new home-manager generation for the current host";
     }
+    {
+      command = let
+        listInputs = "nix flake list-inputs --json <flake=.>";
+
+        getLockedRepos = ".nodes | to_entries | map(select(.key != \"root\")) | map(.value.locked)";
+        toScript = ''map("if [ ! -e \"\(.repo)\" ]\nthen\n  pushd \"<clonebase>\" \\\n  && git clone \"https://github.com/\(.owner)/\(.repo).git\" \\\n  && cd \"\(.repo)\" \\\n  && git checkout \"\(.rev)\" \\\n  && popd\nfi")'';
+        jq = ''jq --raw-output '${getLockedRepos} | ${toScript} | join("\n\n")' '';
+
+        scriptInit = ''echo -ne '#!/usr/bin/env bash\nset -euo pipefail\nmkdir -pv "<clonebase>"\n\n' '';
+      in "__PET_CLONESCRIPT=\"<clonescript=/tmp/clone-flake-inputs.sh>\" && ( ${scriptInit} && ${listInputs} | ${jq}; ) | tee \"$__PET_CLONESCRIPT\" && chmod +x \"$__PET_CLONESCRIPT\" && unset __PET_CLONESCRIPT";
+      description = "Clone the inputs for a nix flake";
+    }
   ];
 }
