ca0fbe5d7654e64105b707d8386b8962249f7d3b
Alexandria Corkwell <i.am.the.memory@gmail.com>
2020-12-30 10:33:09 -0500

    feat(gui): Ensure dbus is started

--- home.refactor/gui/default.nix
+++ home.refactor/gui/default.nix
@@ -7,4 +7,17 @@
     ./kitty.nix
     ./locker.nix
   ];
+
+  home.packages = with pkgs; [
+    # Ensure dbus is available.
+    dbus
+  ];
+
+  # Link dbus.socket into the systemd user directory to ensure it's started for
+  # graphical sessions.
+  home.activation.createDbusSocketLink = inputs.lib.mkSymlink {
+    inherit lib;
+    link = "${config.xdg.configHome}/systemd/user/graphical-session-pre.target.wants/dbus.socket";
+    target = "${config.home.profileDirectory}/etc/systemd/user/dbus.socket";
+  };
 }
