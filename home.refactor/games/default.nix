c58213cf8d4f77bd375366e5cd13aabe3a46f3ff
Alexandria Corkwell <i.am.the.memory@gmail.com>
2021-02-02 17:56:53 -0500

    feat(games): Use only native dependencies for steam-run

--- home.refactor/games/default.nix
+++ home.refactor/games/default.nix
@@ -18,27 +18,35 @@ in
     ./gui.nix
   ];
 
-  home.packages = with pkgs; [
-    # A tool for running appimages.
-    appimage-run
+  home.packages = with pkgs;
+    let
+      # Only use native libraries for steam-run, rather than the older libraries
+      # in its runtime.
+      steam-run = (steam.override {
+        nativeOnly = true;
+      }).run;
+    in
+    [
+      # A tool for running appimages.
+      appimage-run
 
-    # A tool for showing OpenGL info for testing and debugging.
-    glxinfo
+      # A tool for showing OpenGL info for testing and debugging.
+      glxinfo
 
-    # The JAVA runtime.
-    jre
+      # The JAVA runtime.
+      jre
 
-    # A tool for easily running games on Linux.
-    lutris
+      # A tool for easily running games on Linux.
+      lutris
 
-    # A build of WINE using Lutris patches.
-    lutrisWine
+      # A build of WINE using Lutris patches.
+      lutrisWine
 
-    # A tool for running games and other binaries under the same FHS environment
-    # used for Steam.
-    steam-run
+      # A tool for running games and other binaries under the same FHS environment
+      # used for Steam.
+      steam-run
 
-    # A way of easily applying libraries to WINE prefixes.
-    winetricks
-  ];
+      # A way of easily applying libraries to WINE prefixes.
+      winetricks
+    ];
 }
