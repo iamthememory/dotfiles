c89e08b69b4f2c5edeb5560230f982a28329057d
Alexandria Corkwell <i.am.the.memory@gmail.com>
2022-06-07 06:08:22 -0400

    feat(flake): Update inputs

--- home/gui/browser/firefox-addons.nix
+++ home/gui/browser/firefox-addons.nix
@@ -2,10 +2,10 @@
   {
     "adblock-plus" = buildFirefoxXpiAddon {
       pname = "adblock-plus";
-      version = "3.11.4";
+      version = "3.14";
       addonId = "{d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d}";
-      url = "https://addons.mozilla.org/firefox/downloads/file/3872123/adblock_plus-3.11.4-an+fx.xpi";
-      sha256 = "d00391dd04c5b48c0d98c8b058fc2717b82800cd89dd8acadb78e6a44438e82f";
+      url = "https://addons.mozilla.org/firefox/downloads/file/3956140/adblock_plus-3.14.xpi";
+      sha256 = "16b668fa09e888a4574b4d3658456f70554689af63466174aade80849673929d";
       meta = with lib;
       {
         homepage = "https://adblockplus.org/";
@@ -16,10 +16,10 @@
       };
     "adnauseam" = buildFirefoxXpiAddon {
       pname = "adnauseam";
-      version = "3.12.2";
+      version = "3.13.1";
       addonId = "adnauseam@rednoise.org";
-      url = "https://addons.mozilla.org/firefox/downloads/file/3894041/adnauseam-3.12.2-an+fx.xpi";
-      sha256 = "8cdf0d7c505a0a74384cd609a9fabff34ba67ddc033f5299c19973e77fe19692";
+      url = "https://addons.mozilla.org/firefox/downloads/file/3953369/adnauseam-3.13.1.xpi";
+      sha256 = "bdf16fa70f1217c7611c0458024cb5a380c07ac4053be3a03f4bda723d991ebb";
       meta = with lib;
       {
         homepage = "https://adnauseam.io";
@@ -32,7 +32,7 @@
       pname = "downthemall";
       version = "4.3.1";
       addonId = "{DDC359D1-844A-42a7-9AA1-88A850A938A8}";
-      url = "https://addons.mozilla.org/firefox/downloads/file/3871271/downthemall-4.3.1-fx.xpi";
+      url = "https://addons.mozilla.org/firefox/downloads/file/3871271/downthemall-4.3.1.xpi";
       sha256 = "7163f8301a78ab3832563acb3d36aaffcb6be1979e8161dd7de7caecd6e701b6";
       meta = with lib;
       {
@@ -46,7 +46,7 @@
       pname = "retain-me-not";
       version = "1.5.2";
       addonId = "{66da545d-d31d-4eec-a202-622d99b660c7}";
-      url = "https://addons.mozilla.org/firefox/downloads/file/3012883/retain_me_not-1.5.2-fx.xpi";
+      url = "https://addons.mozilla.org/firefox/downloads/file/3012883/retain_me_not-1.5.2.xpi";
       sha256 = "8a77b062526cc4dbd29861eccb908fd04e39d0cdd8e0829a8a7334f7579f8fb6";
       meta = with lib;
       {
@@ -59,7 +59,7 @@
       pname = "zen-fox";
       version = "1.9.9";
       addonId = "{4b7db180-f46c-4955-afaf-a03913751acc}";
-      url = "https://addons.mozilla.org/firefox/downloads/file/3692640/zenfox_solarized_dynamic_lightdark_theme-1.9.9-fx.xpi";
+      url = "https://addons.mozilla.org/firefox/downloads/file/3692640/zen_fox-1.9.9.xpi";
       sha256 = "bac5c909de8f17473ba6823999927e809ee8e9c2165a94f40c1f521a1396cb5e";
       meta = with lib;
       {
