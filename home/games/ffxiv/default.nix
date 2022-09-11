f07c15676c13b82f6698d1cf381b4bfa0c1edbb6
Alexandria Corkwell <i.am.the.memory@gmail.com>
2022-09-11 16:30:14 -0400

    feat(games/ffxiv): Pick newer versions of xivlauncher if available

--- home/games/ffxiv/default.nix
+++ home/games/ffxiv/default.nix
@@ -13,14 +13,45 @@ let
   profileBin = "${config.home.profileDirectory}/bin";
 in
 {
-  home.packages = with inputs.master; [
-    # XIVLauncher.
-    xivlauncher
-  ];
+  home.packages =
+    let
+      # The xivlauncher versions in order of preference.
+      xivlaunchers = [
+        { pkg = pkgs.xivlauncher; source = "pkgs"; priority = 2; }
+        { pkg = inputs.unstable.xivlauncher; source = "unstable"; priority = 1; }
+        { pkg = inputs.master.xivlauncher; source = "master"; priority = 0; }
+      ];
+
+      # A function to sort by version, then priority.
+      # This returns whether a should be preferred over b.
+      versionCompare = a: b:
+        let
+          versionComparison = builtins.compareVersions a.pkg.version b.pkg.version;
+        in
+        if versionComparison == 0
+        then
+          a.priority > b.priority
+        else
+          versionComparison == 1;
+
+      # Pick whichever xivlauncher is best.
+      xivlauncher =
+        let
+          xiv = builtins.elemAt (lib.sort versionCompare xivlaunchers) 0;
+          traceMsg = "Using xivlauncher version " + xiv.pkg.name + " from " + xiv.source + ".";
+        in
+        builtins.trace traceMsg xiv.pkg;
+    in
+    [
+      # XIVLauncher.
+      xivlauncher
+    ];
 
   # Make ACT always floating.
   xsession.windowManager.i3.config.floating.criteria = [
-    { class = "[Aa]dvanced [Cc]ombat [Tt]racker"; }
+    {
+      class = "[Aa]dvanced [Cc]ombat [Tt]racker";
+    }
   ];
 
   # Start FFXIV.
