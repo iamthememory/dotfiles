53ade8751e3904060be0667de75b4d24ecdff4c9
Alexandria Corkwell <i.am.the.memory@gmail.com>
2022-12-18 22:56:06 -0500

    feat(media/sound): Add gpodder

--- home/media/sound/default.nix
+++ home/media/sound/default.nix
@@ -6,19 +6,33 @@
     ./dev.nix
   ];
 
-  home.packages = with pkgs; [
-    # Tools for analyzing music files and submitting data on them to
-    # AcousticBrainz.
-    # Picard uses these.
-    acousticbrainz-client
-    essentia-extractor
+  home.packages =
+    let
+      # Add some additional dependencies to gPodder.
+      gpodder-custom = pkgs.gpodder.overrideAttrs (final: prev: {
+        propagatedBuildInputs =
+          with pkgs.python3Packages; prev.propagatedBuildInputs ++ [
+            mutagen
+            youtube-dl
+          ];
+      });
+    in
+    with pkgs; [
+      # Tools for analyzing music files and submitting data on them to
+      # AcousticBrainz.
+      # Picard uses these.
+      acousticbrainz-client
+      essentia-extractor
 
-    # A tool for tagging music files.
-    picard
+      # A podcast manager.
+      gpodder-custom
 
-    # Tools for working with OGG files and vorbis stuff.
-    vorbis-tools
-  ];
+      # A tool for tagging music files.
+      picard
+
+      # Tools for working with OGG files and vorbis stuff.
+      vorbis-tools
+    ];
 
   # Enable easyeffects for effects on pipewire sound.
   services.easyeffects.enable = true;
