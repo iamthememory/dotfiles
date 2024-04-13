08ea5892ed5bbff4fca91a1c41c7ec98cea9fc05
Alexandria Corkwell <i.am.the.memory@gmail.com>
2024-04-13 02:43:27 -0400

    fix(nixos/pipewire): Convert pipewire settings

--- nixos/sound/pipewire.nix
+++ nixos/sound/pipewire.nix
@@ -1,14 +1,6 @@
 # Configuration for pipewire.
 { ...
 }: {
-  # Configuration for the main pipewire daemon.
-  environment.etc."pipewire/pipewire.d/99-custom-settings.conf".text = ''
-    context.properties = {
-      # Set the sample rate to 96kHz, rather than the default 48kHz.
-      default.clock.rate = 96000
-    }
-  '';
-
   # Enable pipewire.
   services.pipewire.enable = true;
   services.pipewire.audio.enable = true;
@@ -22,6 +14,12 @@
 
   # Enable PulseAudio compatibility for pipewire.
   services.pipewire.pulse.enable = true;
+
+  # Configuration for the main pipewire daemon.
+  services.pipewire.extraConfig.pipewire."99-custom-settings.conf" = {
+    # Set the sample rate to 96kHz, rather than the default 48kHz.
+    "context.properties"."default.clock.rate" = 96000;
+  };
 
   # FIXME: try disabling services.pipewire.socketActivation if pipewire doesn't
   # start properly.
