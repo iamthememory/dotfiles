17528f42f2820d4f936a3ff8dab86e046f9e4b4a
Alexandria Corkwell <i.am.the.memory@gmail.com>
2024-07-20 00:40:20 -0400

    feat(games/gui): Add MangoHud

--- home/games/gui.nix
+++ home/games/gui.nix
@@ -1,8 +1,78 @@
 # GUI-based games and their configurations.
-{ inputs
+{ config
+, inputs
 , pkgs
 , ...
-}: {
+}:
+let
+  mangoHudDefaults = ''
+    # Show the time.
+    time
+    time_format=%a %Y-%m-%d %H:%M:%S %z
+    time_no_label
+
+    # Show CPU info.
+    cpu_stats
+    core_load
+    core_bars
+    cpu_temp
+
+    # Show GPU info.
+    gpu_stats
+    gpu_temp
+
+    # Show memory.
+    procmem
+    ram
+    vram
+    swap
+
+    # Show FPS info.
+    fps
+    frametime
+
+    # Show IO.
+    io_read
+    io_write
+    network=enp3s0,enp4s0,wlp5s0
+
+    # Show wine.
+    wine
+
+    # Show battery info.
+    battery
+
+    # Show throttling.
+    throttling_status
+
+    # Show frame timing.
+    frame_timing
+
+    # Use LiterationMono.
+    font_file=${config.home.profileDirectory}/share/fonts/truetype/NerdFonts/LiterationMonoNerdFontMono-Regular.ttf
+    font_size=16
+
+    # Use a more compact HUD.
+    hud_compact
+
+    # Outline text.
+    text_outline
+
+    # Increase transparency.
+    background_alpha = 0.2
+    alpha = 0.4
+
+    # Reload config.
+    reload_cfg=Shift_L+F4
+
+    # Toggle HUD position.
+    toggle_hud_position=Shift_R+F11
+
+    # Toggle HUD.
+    toggle_hud=Shift_R+F12
+  '';
+in
+{
   home.packages =
     let
       # Build Cataclysm: DDA from the current source.
@@ -62,6 +132,9 @@
       # Freeciv.
       freeciv_gtk
 
+      # A game overlay.
+      mangohud
+
       # A Minecraft launcher and manager.
       prismlauncher
 
@@ -74,4 +147,11 @@
       # A Minecraft-like survival game.
       vintagestory
     ];
+
+  xdg.configFile."MangoHud/MangoHud.conf".text = mangoHudDefaults;
+
+  xdg.configFile."MangoHud/wine-eldenring.conf".text = mangoHudDefaults + ''
+    # Show the HUD in the top-right.
+    position=top-right
+  '';
 }
