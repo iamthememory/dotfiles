4a85127b3f7801a4642c4769b311fcc38a597d9b
Alexandria Corkwell <i.am.the.memory@gmail.com>
2024-09-30 17:24:37 -0400

    feat(overlay): Add 8BitDo controller config to retroarch

--- overlay/default.nix
+++ overlay/default.nix
@@ -9,6 +9,13 @@ self: super: {
   # own completions take priority.
   nix-zsh-completions = super.lib.setPrio 20 super.nix-zsh-completions;
 
+  # Add the configuration for the 8BitDo Pro 2 controller.
+  retroarch-joypad-autoconfig = super.retroarch-joypad-autoconfig.overrideAttrs {
+    patches = [
+      ./retroarch-8bitdo.patch
+    ];
+  };
+
   # Customize steam.
   steam = import ./steam.nix { inherit self super; };
 }
