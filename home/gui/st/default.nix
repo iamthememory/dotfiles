5b187d501ceeec51b3492b097b3c0107d586e2ba
Alexandria Corkwell <i.am.the.memory@gmail.com>
2025-04-29 23:41:23 -0400

    fix(st): Replace obsolete substituteAll

--- home/gui/st/default.nix
+++ home/gui/st/default.nix
@@ -6,15 +6,9 @@
 let
   st = pkgs.st.overrideAttrs (oldAttrs: {
     # Override the default configuration for st with a customized one.
-    configFile = pkgs.substituteAll {
+    configFile = pkgs.replaceVars ./config.h {
       # Substitute @default_shell@ with ZSH in our current profile.
       default_shell = "${config.home.profileDirectory}/bin/zsh";
-
-      # The name of the file to generate.
-      name = "config.h";
-
-      # The file to substitute in.
-      src = ./config.h;
     };
   });
 in
