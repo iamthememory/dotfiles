9aedb0716e9619fe17d88bfaf5e2397ec2a284ab
Alexandria Corkwell <i.am.the.memory@gmail.com>
2025-10-06 16:10:55 -0400

    fix(ssh): Migrate obsolete options

--- home/ssh.nix
+++ home/ssh.nix
@@ -1,5 +1,6 @@
 # Configuration for the SSH client.
-{ pkgs
+{ config
+, pkgs
 , ...
 }: {
   home.packages = with pkgs; [
@@ -9,23 +10,32 @@
 
   # Enable SSH's configuration.
   programs.ssh.enable = true;
+  programs.ssh.enableDefaultConfig = false;
 
   # Enable SSH compression.
-  programs.ssh.compression = true;
+  programs.ssh.matchBlocks."*".compression = true;
 
   # Multiplex multiple sessions over a single connection when possible.
-  programs.ssh.controlMaster = "auto";
+  programs.ssh.matchBlocks."*".controlMaster = "auto";
 
   # The path to the control socket when multiplexing sessions.
-  programs.ssh.controlPath = "~/.ssh/control-%r@%h:%p";
+  programs.ssh.matchBlocks."*".controlPath =
+    "${config.home.homeDirectory}/.ssh/control-%r@%h:%p";
 
   # Keep control sockets open in the background for ten minutes.
-  programs.ssh.controlPersist = "10m";
+  programs.ssh.matchBlocks."*".controlPersist = "10m";
+
+  # Don't forward local authentication to the remote side.
+  programs.ssh.matchBlocks."*".forwardAgent = false;
 
   # Hash hostnames and addresses when adding them to the known hosts file to
   # lessen information leaking.
-  programs.ssh.hashKnownHosts = true;
+  programs.ssh.matchBlocks."*".hashKnownHosts = true;
 
   # Send a keepalive every 30 seconds.
-  programs.ssh.serverAliveInterval = 30;
+  programs.ssh.matchBlocks."*".serverAliveInterval = 30;
+
+  # The file for known hosts.
+  programs.ssh.matchBlocks."*".userKnownHostsFile =
+    "${config.home.homeDirectory}/.ssh/known_hosts";
 }
