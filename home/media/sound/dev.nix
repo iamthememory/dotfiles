58414447664745f0ec18859310bf8a03b0dd3761
Alexandria Corkwell <i.am.the.memory@gmail.com>
2026-02-08 14:50:27 -0500

    feat(media): Add audio plugins

--- home/media/sound/dev.nix
+++ home/media/sound/dev.nix
@@ -1,6 +1,5 @@
 # Configuration for making and editting music and sound files.
 { config
-, inputs
 , pkgs
 , ...
 }:
@@ -26,11 +25,20 @@ in
     # A host for audio plugins.
     carla
 
+    # A synthesizer.
+    fluidsynth
+
+    # A sampler.
+    linuxsampler
+
     # A digital audio workstation.
     lmms
 
     # A tool to control a running JACK session and its connections.
     qjackctl
+
+    # A synthesizer.
+    qsynth
 
     # A set of tools for low-level editing of sound files.
     sox
@@ -38,45 +46,166 @@ in
 
     # A variety of audio plugins.
     # Search for more plugins:
-    # nix-locate --minimal --at-root -t d -t s -r '/lib/(dssi|ladspa|lv2|lxvst|vst)' | sort | uniq | sed 's@\.out$@@'
+    # nix-locate --minimal --at-root -t d -t s -r '/lib/(dssi|ladspa|lv2|lxvst|vst)' | sort | uniq | sed -e 's@\.out$@@' -e 's@^@    @'
     adlplug
     aeolus
+    aether-lv2
+    airwin2rack
+    airwindows
+    airwindows-lv2
     AMB-plugins
     artyFX
-    bristol
+    autotalent
+    bankstown-lv2
+    bchoppr
+    bjumblr
+    bolliedelayxt-lv2
+    boops
+    #bristol
+    bs2b-lv2
+    bschaffl
+    bsequencer
     bshapr
     bslizr
     calf
     caps
+    cardinal
     carla
+    chow-centaur
+    chow-kick
+    chow-phaser
+    chow-tape-model
     cmt
     csa
+    ctagdrc
+    deepfilternet
+    delayarchitect
+    dexed
+    diopser
     distrho-ports
+    dragonfly-reverb
     drumgizmo
     drumkv1
+    dsp
     dssi
+    ensemble-chorus
     eq10q
-    fluidsynth
+    faustPhysicalModeling
+    fil-plugins
+    filtr
+    fire
+    fluida-lv2
     fomp
+    fverb
+    gate12
+    geonkick
+    guitarix
+    gxmatcheq-lv2
     gxplugins-lv2
     helm
+    hybridreverb2
     infamousPlugins
     ingen
+    ir-lv2
+    kapitonov-plugins-pack
     ladspaPlugins
-    linuxsampler
+    ladspa-sdk
+    librearp-lv2
+    librearp
+    lsp-plugins
+    lv2
+    magnetophonDSP.CharacterCompressor
+    magnetophonDSP.CompBus
+    magnetophonDSP.ConstantDetuneChorus
+    magnetophonDSP.faustCompressors
+    magnetophonDSP.LazyLimiter
+    magnetophonDSP.MBdistortion
+    magnetophonDSP.pluginUtils
+    magnetophonDSP.RhythmDelay
+    magnetophonDSP.shelfMultiBand
+    magnetophonDSP.VoiceOfFaust
+    master_me
     mda_lv2
-    metersLv2
+    melmatcheq-lv2
+    #metersLv2
+    midi-trigger
+    mod-arpeggiator-lv2
     mod-distortion
+    molot-lite
+    mooSpace
+    neural-amp-modeler-lv2
+    ninjas2
+    noise-repellent
     nova-filters
-    qsynth
+    odin2
+    open-music-kontrollers.eteroj
+    open-music-kontrollers.jit
+    open-music-kontrollers.mephisto
+    open-music-kontrollers.midi_matrix
+    open-music-kontrollers.moony
+    open-music-kontrollers.orbit
+    open-music-kontrollers.router
+    open-music-kontrollers.sherlock
+    open-music-kontrollers.synthpod
+    open-music-kontrollers.vm
+    opnplug
+    plugdata
+    plujain-ramp
+    proteus
+    qdelay
+    qmidiarp
+    quadrafuzz
+    reevr
+    ripplerx
+    rkrlv2
+    rnnoise-plugin.ladspa
+    rnnoise-plugin.lv2
+    rnnoise-plugin.lxvst
+    rnnoise-plugin
+    rnnoise-plugin.vst3
+    rubberband
     setbfree
+    sfizz-ui
+    sg-323
+    show-midi
+    socalabs-papu
+    socalabs-rp2a03
+    socalabs-sid
+    socalabs-sn76489
+    socalabs-voc
+    sonobus
     sorcer
+    spectmorph
     speech-denoiser
+    stochas
+    stone-phaser
+    string-machine
+    #surge
+    surge-XT
     swh_lv2
     synthv1
+    talentedhack
+    tambura
+    tamgamp-lv2
+    tap-plugins
+    time12
+    tunefish
+    uhhyou-plugins-juce
+    uhhyou-plugins
+    vaporizer2
+    vocproc
+    wolf-shaper
     x42-avldrums
+    x42-gmsynth
+    x42-plugins
     yoshimi
+    ysfx
     zam-plugins
+    zlcompressor
+    zlequalizer
+    zlsplitter
+    #zynaddsubfx-fltk
+    #zynaddsubfx-ntk
     zynaddsubfx
   ];
 
