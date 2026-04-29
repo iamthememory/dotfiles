bde3bc0f4a6278b358e89570b112be42148d0d59
Alexandria Corkwell <i.am.the.memory@gmail.com>
2026-04-29 14:15:27 -0400

    feat(CDDA): Tweak vending

--- packages/cataclysm-dda-git-with-mods/default.nix
+++ packages/cataclysm-dda-git-with-mods/default.nix
@@ -74,6 +74,7 @@ let
       ./patches/cataclysm-perks-fix.patch
       ./patches/cataclysm-plant-in-cold.patch
       ./patches/cataclysm-plant-only-check-now.patch
+      ./patches/cataclysm-powered-vending.patch
       ./patches/cataclysm-seedbearer-fix.patch
       ./patches/cataclysm-spell-ui-widen.patch
       ./patches/cataclysm-start-with-no-money.patch
