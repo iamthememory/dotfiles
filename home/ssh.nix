e283c30da405b684c7fca1d24e05e1d917160a3d
Alexandria Corkwell <i.am.the.memory@gmail.com>
2026-05-23 10:47:29 -0400

    fix(ssh): Migrate ssh settings

--- home/ssh.nix
+++ home/ssh.nix
@@ -13,30 +13,30 @@
   programs.ssh.enableDefaultConfig = false;
 
   # Enable SSH compression.
-  programs.ssh.matchBlocks."*".compression = true;
+  programs.ssh.settings."*".compression = true;
 
   # Multiplex multiple sessions over a single connection when possible.
-  programs.ssh.matchBlocks."*".controlMaster = "auto";
+  programs.ssh.settings."*".controlMaster = "auto";
 
   # The path to the control socket when multiplexing sessions.
-  programs.ssh.matchBlocks."*".controlPath =
+  programs.ssh.settings."*".controlPath =
     "${config.home.homeDirectory}/.ssh/control-%r@%h:%p";
 
   # Keep control sockets open in the background for ten minutes.
-  programs.ssh.matchBlocks."*".controlPersist = "10m";
+  programs.ssh.settings."*".controlPersist = "10m";
 
   # Don't forward local authentication to the remote side.
-  programs.ssh.matchBlocks."*".forwardAgent = false;
+  programs.ssh.settings."*".forwardAgent = false;
 
   # Hash hostnames and addresses when adding them to the known hosts file to
   # lessen information leaking.
-  programs.ssh.matchBlocks."*".hashKnownHosts = true;
+  programs.ssh.settings."*".hashKnownHosts = true;
 
   # Send a keepalive every 30 seconds.
-  programs.ssh.matchBlocks."*".serverAliveInterval = 30;
+  programs.ssh.settings."*".serverAliveInterval = 30;
 
   # The file for known hosts.
-  programs.ssh.matchBlocks."*".userKnownHostsFile =
+  programs.ssh.settings."*".userKnownHostsFile =
     "${config.home.homeDirectory}/.ssh/known_hosts";
 
   # Enable ssh-agent.
